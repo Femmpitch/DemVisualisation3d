3aef648356c07dcfbeda16f6dce891ccf184d399
Fedor Pesyak <fedorpesyak@gmail.com>
2017-04-17 16:38:42 +0300

    Edited X and Y coord matrices, added scale factor

--- DEM_visualisation.py
+++ DEM_visualisation.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 import gdal, osr
 import numpy as np
 import visvis
@@ -9,8 +9,8 @@ def importDemAsArray(path, xsize=None, ysize=None, xoff=0, yoff=0):
     Программа открывает файл DEM формата GeoTIFF и считывает с него геоинформацию и матрицу высот.
     Входной файл должен быть записан в системе координат WGS-84 (самы распространенный формат,
     позже реализуем для любого).
-    Входной dataset преобразуется в систему координат EPSG:3857 - Pseudo Mercator (прямоугольная система с учетом
-    кривизны Земли)
+    Входной dataset преобразуется в систему координат EPSG:3395 - Pseudo Mercator (прямоугольная система с учетом
+    кривизны Земли в метрах)
     xoff, yoff - начальные индексы среза массива, xsize, ysize - размеры массива
     Осторожнее с большими файлами - может зависнуть. Для подобных файлов используйте дополнительные входные параметры
     (пример 2).
@@ -20,7 +20,7 @@ def importDemAsArray(path, xsize=None, ysize=None, xoff=0, yoff=0):
 
     # Задаем старую и новую системы координат
     inputEPSG = 4326  # WGS-84
-    outputEPSG = 3857  # Pseudo Mercator
+    outputEPSG = 3395  # World Mercator (units meters)
     inSpatialRef = osr.SpatialReference()
     inSpatialRef.ImportFromEPSG(inputEPSG)
     outSpatialRef = osr.SpatialReference()
@@ -34,36 +34,36 @@ def importDemAsArray(path, xsize=None, ysize=None, xoff=0, yoff=0):
     # Чтение высот как матрицы
     Z = dataset.ReadAsArray(xoff, yoff, xsize, ysize)
     #  определяем массивы координат x и y
-    x = np.linspace(0, Z.shape[1], Z.shape[1])
-    y = np.linspace(0, Z.shape[0], Z.shape[0])
+    x = np.linspace(0, Z.shape[1]-1, Z.shape[1])
+    y = np.linspace(0, Z.shape[0]-1, Z.shape[0])
     # Делаем meshgrid: X,Y
-    X, Y = np.meshgrid(x, y)
+    X, Y = np.meshgrid(x, y )
     # Приведение Z к нулю в центре
     Z = Z - Z[Z.shape[0] / 2, Z.shape[1] / 2]
     # Преобразование X и Y с помощью geotransform
-    X, Y = gt[0] + X + gt[2] * Y, gt[3] + gt[4] * X + Y
+    X, Y = gt[0] + gt[1]*X + gt[2] * Y, gt[3] + gt[4] * X + gt[5]*Y
     # Центрирование координат X и Y
-    X, Y = X - X[X.shape[0] / 2, X.shape[1] / 2], Y - Y[Y.shape[0] / 2, Y.shape[1] / 2]
+    # X, Y = X - X[X.shape[0] / 2, X.shape[1] / 2], Y - Y[Y.shape[0] / 2, Y.shape[1] / 2]
     return X, Y, Z
 
-def visualizeDEM(X, Y, Z):
+def visualizeDEM(X, Y, Z, scale=1.0):
     """Создание 3D-модели из вычисленных ранее массивов"""
-    m = visvis.surf(X, Y, Z)
+    m = visvis.surf(X, Y, scale*Z)
     m.colormap = visvis.CM_JET
     app = visvis.use()
     app.Run()
 
-def visualizeDEM_direct(path):
+def visualizeDEM_direct(path, scale=1.0):
     """Создание 3D-модели без сохранения массивов в памяти"""
     X, Y, Z = importDemAsArray(path)
-    visualizeDEM(X, Y, Z)
+    visualizeDEM(X, Y, Z, scale)
 
 
 if __name__ == "__main__":
     #1. Пример 1
     path = 'n55_e037_3arc_v1.tif'
     X, Y, Z = importDemAsArray(path)
-    visualizeDEM(X, Y, Z)
+    visualizeDEM(X, Y, Z, 20)
 
     #2. Пример 2
     # path = 'srtm_germany_dsm.tif'
